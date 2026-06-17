632db9cfcf397c15393c2cc6bb5e0e3c2d1d1a4b
Akshita Sharma <sakshita229@gmail.com>
2026-06-17 20:24:15 +0530

    model training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging 
+from src.logger import logging
 
 '''
 just created a custom exception class that behaves like a normal python error but also stores file name and line number where the error happened 
